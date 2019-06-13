2183e64ca418999d12f727b3e1248129285cc9dd
Chris Lee <chrislee@LeeCMBP.local>
2019-06-12 22:20:40 -0400

    m-th smallest number WIP
    study Big O notation

--- SortNumber/Program.cs
+++ SortNumber/Program.cs
@@ -1,10 +1,8 @@
 ﻿using System;
 
-using System;
-
 namespace SortNumber
 {
-    class Program
+  class Program
     {
         static void Main(string[] args)
         {
@@ -13,6 +11,9 @@ namespace SortNumber
             Console.WriteLine(smallestNumber(nums));
 
             Console.WriteLine("Second smallest number is : {0}", secondSmallestNumber(nums));
+
+            int m = 4;
+            Console.WriteLine("{0}th smallest number is: {1}", m, mthSmallestNumber(nums, m));
         }
 
         public static int smallestNumber(int[] numbers)
@@ -46,6 +47,39 @@ namespace SortNumber
                 }
             }
             return secondSmallest; 
+        }
+
+        /**
+        *   3, 1, 9, 8, 6, 5
+        *   3, 1, 9, 8, 6, 5
+        *   loop through the array
+        *       find smallest number
+        */
+        public static int mthSmallestNumber(int[] numbers, int m)
+        {
+            int[] sortedNumbers = new int[m];
+            /* loop to mth position */
+            int[] newNumbers = new int[numbers.Length];
+            string original = numbers.ToString();
+            string neww = original;
+            int[] newww = neww.Split(',');
+            for (int i=0; i<m; i++)
+            {
+                int sn = smallestNumber(newNumbers);
+                sortedNumbers.SetValue(sn, i);
+                int k=0;
+                for (int j=0; j< numbers.Length; j++)
+                {
+                    if (numbers[j] != sn)
+                    {
+                        newNumbers[k] = numbers[j];
+                    } else {
+                        j++;
+                    }
+                    k++;
+                }
+            }
+            return sortedNumbers[m-1];
         }
     }
 }
